57ba7bc8034d73e2c6b70e7bcf6ca6694721028c
taufiqsultan <nirul.sultan@gmail.com>
2023-08-23 16:05:58 -0300

    API commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
-import api.DeckOfCards;
 import interfaces.PlayerEventListener;
-import model.Card;
-import model.Deck;
-import model.Player;
 import service.GameManager;
 import service.InputManager;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         InputManager inputManager = new InputManager();
         GameManager gameManager = new GameManager();
+
 
         PlayerEventListener playerEventListener = new PlayerEventListener() {
             @Override
@@ -21,29 +16,15 @@ public class Main {
         };
 
         gameManager.setPlayerEventListener(playerEventListener);
-
         while (true) {
-            // Create a new deck and player for each game
-            Deck deck = Deck.createNewDeck();
-            Player player = new Player("Player");
-
-            // Draw cards for the player
-            try {
-                player.dealCardsFromDeck(Deck.getDeckId(), 2); // Assuming you want to draw 2 cards
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            System.out.println("Player's dealt cards: " + player.getDealtCards());
-
+            gameManager.startGame();
             System.out.println("Do you want to play again? (y/n)");
-            String answer = inputManager.getPlayerDecision();
+            String answer = InputManager.getPlayerDecision(); // Use getPlayerDecision() here
             if (answer.equals("n")) {
                 break;
             }
-            player.clearDealtCards(); // Clear dealt cards for the next game
         }
-
-        inputManager.closeScanner();
+        inputManager.closeScanner(); // Close the scanner after the loop
     }
+
 }
